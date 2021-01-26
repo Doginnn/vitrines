935e75492bc632707479791efeccbf17857f768c
doginnn <diogenesemmanuel@gmail.com>
2021-01-26 15:44:39 -0300

    [UPD]Refatorando a API para DRF.

--- vitrines/vitrines/views.py
+++ vitrines/vitrines/views.py
@@ -1,6 +1,2 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render
 
-
-def index(request):
-    return HttpResponse('Teste SBTur!')

2b38c349f83c6c34604400abbe282e9fe2db9608
doginnn <diogenesemmanuel@gmail.com>
2021-01-27 12:10:20 -0300

    [UPD]Atualizando o urls.py

--- vitrines/urls.py
+++ vitrines/urls.py
@@ -21,5 +21,7 @@ from .router import router
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('item', include(router.urls)),
+    # path('/', tem que me retornar o primeiro e segundo pacote criado "id 1 e id 2"),
+    # path('/destinos', tem que me retornar o primeiro pacote criado "id 1"),
+    # path('/sobre', tem que me retornar uma lista vazia "[]"),
 ]

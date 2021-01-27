3a1b8897a04c4588adf1296b363111f308c2c6fb
doginnn <diogenesemmanuel@gmail.com>
2021-01-27 12:12:23 -0300

    [UPD]Refatorando o serializers.py

--- vitrines/vitrines/serializers.py
+++ vitrines/vitrines/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
-from vitrines.vitrines.models import Hotel, Item
+from vitrines.vitrines.models import Title, Hotel
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Title
+        fields = '__all__'
 
 
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
-        fields = '__all__'
-
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
         fields = '__all__'

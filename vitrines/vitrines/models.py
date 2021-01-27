16d63c4cd9fc57e3ebc2d2c8d3ad86785150f055
doginnn <diogenesemmanuel@gmail.com>
2021-01-27 12:11:28 -0300

    [UPD]Refatorando o DB.

--- vitrines/vitrines/models.py
+++ vitrines/vitrines/models.py
@@ -1,19 +1,14 @@
 from django.db import models
 
 
+#Classe que mostra a Vitrine(Pacote)
+class Title(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Pacotes')
+    subtitle = models.CharField(max_length=200, verbose_name='Descrição')
+
+
 class Hotel(models.Model):
-    title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.title
-
-
-class Item(models.Model):
-    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
-    hotel_name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=100)
-    image = models.URLField()
-
-    def __str__(self):
-        return self.hotel_name
+    hotel_name = models.CharField(max_length=100, verbose_name='Nome do Hotel')
+    slug = models.CharField(max_length=100, verbose_name='Apelido do Hotel')
+    image = models.URLField(verbose_name='Imagem')
+    price = models.IntegerField(verbose_name='Valor R$')

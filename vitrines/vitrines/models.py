3aa879c000f126e6e7751c9d7a6ade6a8c698539
doginnn <diogenesemmanuel@gmail.com>
2021-01-27 15:24:56 -0300

    [UPD]Criando modelo Category e refatorando as outras classes slug >> SlugField.

--- vitrines/vitrines/models.py
+++ vitrines/vitrines/models.py
@@ -9,17 +9,22 @@ class Title(models.Model):
 
 class Hotel(models.Model):
     hotel_name = models.CharField(max_length=100, verbose_name='Nome do Hotel')
-    slug = models.CharField(max_length=100, verbose_name='Apelido do Hotel')
+    slug = models.SlugField(verbose_name='Apelido do Hotel')
     image = models.URLField(verbose_name='Imagem')
     price = models.IntegerField(verbose_name='Valor R$')
 
 
 class City(models.Model):
     name = models.CharField(max_length=50, verbose_name='Nome da cidade')
-    slug = models.CharField(max_length=50, verbose_name='Apelido da cidade')
+    slug = models.SlugField(verbose_name='Apelido da cidade')
     state = models.CharField(max_length=50, verbose_name='Abreviação do estado')
 
 
 class Country(models.Model):
     name = models.CharField(max_length=50, verbose_name='Nome do País')
-    slug = models.CharField(max_length=50, verbose_name='Apelido do país')
+    slug = models.SlugField(verbose_name='Apelido do país')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nome do País')
+    slug = models.SlugField(verbose_name='Apelido do país')

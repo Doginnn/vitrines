fe26fee6c87d26dd417190d428a534ba85e86ee3
doginnn <diogenesemmanuel@gmail.com>
2021-01-28 17:53:19 -0300

    [DEL|UPD]Transferindo arquivos viewsets.py >> views.py

--- vitrines/urls.py
+++ vitrines/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_nested import routers
-from vitrines.vitrines.viewsets import EventoViewSet, ItemViewSet, CityViewSet
+from vitrines.vitrines.views import EventoViewSet, ItemViewSet, CityViewSet
 
 
 api_router = routers.DefaultRouter()

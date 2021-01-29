0aed7318b0216ebdef2c9db74af7205c6f775dfd
doginnn <diogenesemmanuel@gmail.com>
2021-01-29 01:15:24 -0300

    [UPD]Atualizando urls.py

--- vitrines/urls.py
+++ vitrines/urls.py
@@ -1,24 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_nested import routers
-from vitrines.vitrines.views import EventoViewSet, ItemViewSet, CityViewSet
+from vitrines.vitrines.views import EventoViewSet, ItemViewSet, CityViewSet, CountryViewSet, CategoryViewSet
 
-
+# Routes from API
 api_router = routers.DefaultRouter()
 api_router.register(r'eventos', EventoViewSet)
+# api_router.register(r'item', ItemViewSet)
+api_router.register(r'city', CityViewSet)
+api_router.register(r'country', CountryViewSet)
+api_router.register(r'category', CategoryViewSet)
 
-eventos_router = routers.NestedDefaultRouter(api_router, r"eventos", lookup="evento")
+# Routes from REST_FRAMEWORK_NESTED
+eventos_router = routers.NestedDefaultRouter(api_router, r"eventos", lookup='evento')
 eventos_router.register(r"items", ItemViewSet, basename="evento-items")
-
-# citys_router = routers.NestedDefaultRouter(api_router, r"citys", lookup="city")
-# citys_router.register(r"citys", CityViewSet, basename="city-items")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(api_router.urls)),
-    path('', include(eventos_router.urls)),
-    # path('', include(citys_router.urls)),
+    path('item/', include(api_router.urls)),
+    path('city/', include(api_router.urls)),
+    path('country/', include(api_router.urls)),
+    path('category/', include(api_router.urls)),
 ]
 
 # path('/', tem que me retornar o primeiro e segundo pacote criado "id 1 e id 2"),

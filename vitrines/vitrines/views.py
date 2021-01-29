9d3977dd3f8c3f20195689a3e28e70bf43e5be4f
doginnn <diogenesemmanuel@gmail.com>
2021-01-29 12:09:31 -0300

    [RFT]Refatorando views.py

--- vitrines/vitrines/views.py
+++ vitrines/vitrines/views.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets
 from vitrines.vitrines.serializers import *
 
 
-# Lista todas as vitrines
 class EventoViewSet(viewsets.ModelViewSet):
     serializer_class = EventoSerializer
     queryset = Evento.objects.all()
@@ -11,11 +10,6 @@ class EventoViewSet(viewsets.ModelViewSet):
 class ItemViewSet(viewsets.ModelViewSet):
     serializer_class = ItemSerializer
     queryset = Item.objects.all()
-
-    # def get_queryset(self):
-    #     return Item.objects.filter(
-    #         evento=self.kwargs['evento_pk']
-    #     )
 
 
 class CityViewSet(viewsets.ModelViewSet):
@@ -31,9 +25,3 @@ class CountryViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
-
-# Criar a viewset para retornar a vitrine - CRUD;
-# Criar a viewset para listar toda a vitrine - OK; ('')
-# Criar a viewset para retornar a primeira e segunda vitrine criada "id 1 e id 2"; ('/')
-# Criar a viewset para listar uma vitrine por ID; ('vitrines/destinos'); ('vitrines/destinos')
-# Criar a viewset para retornar uma lista vazia; ('vitrines/sobre')

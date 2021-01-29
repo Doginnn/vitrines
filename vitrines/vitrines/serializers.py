27db721d64213b1c91cdd6d6681c9741790de02b
doginnn <diogenesemmanuel@gmail.com>
2021-01-29 01:16:58 -0300

    [UPD]Atualizando serializers.py

--- vitrines/vitrines/serializers.py
+++ vitrines/vitrines/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from vitrines.vitrines.models import Evento, Item, City
+from vitrines.vitrines.models import *
 
 
 class EventoSerializer(serializers.ModelSerializer):
@@ -17,4 +17,16 @@ class ItemSerializer(serializers.ModelSerializer):
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
+        fields = '__all__'
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
         fields = '__all__'
